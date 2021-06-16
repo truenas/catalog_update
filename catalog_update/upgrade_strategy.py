3f6d9d5530206fec77d2b11b4be972a217569070
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-06-16 11:32:02 +0500

    Add datetime versioning helper

--- catalog_update/upgrade_strategy.py
+++ catalog_update/upgrade_strategy.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pkg_resources import parse_version
 from pkg_resources.extern.packaging.version import Version
 from typing import Optional
@@ -6,7 +7,16 @@ from typing import Optional
 def semantic_versioning(tags: list) -> Optional[str]:
     versions = [v for v in map(parse_version, tags) if isinstance(v, Version) and not v.is_prerelease]
     if not versions:
-        return None
+        return
 
     versions.sort()
     return str(versions[-1])
+
+
+def datetime_versioning(tags: list, date_format: str) -> Optional[str]:
+    versions = [datetime.strptime(t, date_format) for t in tags]
+    if not versions:
+        return
+
+    versions.sort()
+    return versions[-1].strftime(date_format)
